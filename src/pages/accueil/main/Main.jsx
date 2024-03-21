632fbec28a6fa28737b8bd9c5bdd5676d5c65a9d
KaynoxDev <r.chauvard18@gmail.com>
2024-03-21 16:17:26 +0100

    Structure main + composant github pour plus tard

--- src/pages/accueil/main/Main.jsx
+++ src/pages/accueil/main/Main.jsx
@@ -13,6 +13,25 @@ function Main() {
           </button>
         </div>
       </div>
+
+      <section className="apropos">
+        <div className="container-fluid">
+          <div className="row-col-2">
+            <article className="col">
+              <h2 className="title">à propos</h2>
+              <p>
+                Passionné par l'informatique et les nouvelles technologies, j'ai
+                suivi une formation d'
+                <strong>intégrateur-développeur web au CEF</strong>. Au cours de
+                cette formation, j'ai pu acquérir des bases solides pour
+                travailler dans le domaine du <strong>développement web</strong>
+                .
+              </p>
+            </article>
+            <article className="col"></article>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
